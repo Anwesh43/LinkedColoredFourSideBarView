f494b785eb2d523a47c31ec3c0c9896138e6aa9c
Anwesh43 <anweshthecool0@gmail.com>
2020-06-29 03:45:11 +0530

    adding CFSBNode class

--- coloredfoursidebarview/src/main/java/com/anwesh/uiprojects/coloredfoursidebarview/ColoredFourSideBarView.kt
+++ coloredfoursidebarview/src/main/java/com/anwesh/uiprojects/coloredfoursidebarview/ColoredFourSideBarView.kt
@@ -120,4 +120,45 @@ class ColoredFourSideBarView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class CFSBNode(var i : Int, val state : State = State()) {
+
+        private var next : CFSBNode? = null
+        private var prev : CFSBNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = CFSBNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawCFSBNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : CFSBNode {
+            var curr : CFSBNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
